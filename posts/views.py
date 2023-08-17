a90a6129b2b598eef437811a5b252f983a4c080f
corevm <charfull.mail@gmail.com>
2023-08-17 20:17:29 +0300

    ALL CRUD + ALL Validate + Admin

--- posts/views.py
+++ posts/views.py
@@ -4,20 +4,19 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import UpdateView
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
 from posts.models import Post
+from posts.permissions import PostOwnerPermission
 from posts.serializers import PostCreateSerializer, PostListSerializer, PostDetailSerializer, PostUpdateSerializer, \
     PostDestroySerializer
-
-
-
 
 
 class PostCreateView(CreateAPIView):
     model = Post
     serializer_class = PostCreateSerializer
-    # permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer: PostCreateSerializer) -> None:
         with transaction.atomic():
@@ -27,20 +26,22 @@ class PostCreateView(CreateAPIView):
 class PostListView(ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostListSerializer
-    # permission_classes = [IsAdminUser]
+    permission_classes = [AllowAny]
 
 
 class PostDetailView(RetrieveAPIView):
     queryset = Post.objects.all()
     serializer_class = PostDetailSerializer
-    # permission_classes = [IsAdminUser | UserOwnerPermission]
+    permission_classes = [AllowAny]
 
 class PostUpdateView(UpdateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostUpdateSerializer
-    # permission_classes = [IsAd
+    permission_classes = [IsAdminUser | PostOwnerPermission]
 
     def pacth(self, request, *args, **kwargs):
+        file = request.data.get('images', None)
+        images = Post.objects.create(images=file)
         # serializer = Reader.objects.all()
         s: PostUpdateSerializer = self.get_serializer(data=request.data)
         if s.is_valid():
@@ -51,24 +52,25 @@ class PostUpdateView(UpdateAPIView):
 class PostDeleteView(DestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostDestroySerializer
-    # permission_classes = [IsAdminUser]
+    permission_classes = [IsAdminUser | PostOwnerPermission]
 
 
 @method_decorator(csrf_exempt, name='dispatch')
 class PostImageView(UpdateView):
     model = Post
     fields = ['title', 'text', 'images']
+    permission_classes = [IsAdminUser | PostOwnerPermission]
 
     def post(self, request, *args, **kwargs):
         self.object = self.get_object()
-        self.object.photo = request.FILES['images']
+        self.object.images = request.FILES['images']
         self.object.save()
 
         return JsonResponse({
             'id': self.object.id,
-            'title': self.object.name,
-            'text': self.object.last_name,
+            'title': self.object.title,
+            'text': self.object.text,
             'created_at': self.object.created_at,
             'update_at': self.object.update_at,
-            'images': self.object.photo.url if self.object.photo else None
+            'images': self.object.images.url if self.object.images else None
         })
